2a7defb82a7d1ceee8a81158a9c190e403ea0330
niilz <niilz@outlook.de>
2026-03-22 15:46:03 +0100

    Migrate to AGP 9.X following Philipp Lackner YouTube
     https://youtu.be/Jp3Yg1VSRkY?si=-mLNYkUsdk9TBcYB

--- androidApp/src/main/java/de/niilz/kmp/androidapp/MainActivity.kt
+++ androidApp/src/main/java/de/niilz/kmp/androidapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package de.niilz.kmp.workoutlog2
+package de.niilz.kmp.androidapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import de.niilz.kmp.workoutlog2.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -3,18 +3,20 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.androidKmpLibrary)
 }
 
 kotlin {
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
+
+    androidLibrary {
+        compileSdk = 36
+        minSdk = 26
+        namespace = "de.niilz.kmp.workoutlog2.composeApp"
+        experimentalProperties["android.experimental.kmp.enableAndroidResources"] = true
     }
-    
+
     listOf(
         iosArm64(),
         iosSimulatorArm64()
@@ -45,35 +47,3 @@ kotlin {
         }
     }
 }
-
-android {
-    namespace = "de.niilz.kmp.workoutlog2"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    defaultConfig {
-        applicationId = "de.niilz.kmp.workoutlog2"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-dependencies {
-    debugImplementation(libs.compose.uiTooling)
-}
-
